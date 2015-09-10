6e75295932ace7f8bc590d998d2949f84f145b60
Will Dixon <dixonwille@hotmail.com>
2015-09-10 02:01:38 -0400

    Implemented Cache for API

--- Contact Book/app/factories/api.js
+++ Contact Book/app/factories/api.js
@@ -2,8 +2,9 @@
     'use strict';
     angular.module('cbApp')
         .factory('api', api);
-    api.$inject = ['$http','logger','$q','auth'];
-    function api($http,logger,$q,auth) {
+    api.$inject = ['$http','logger','$q','$cacheFactory','auth'];
+    function api($http, logger, $q, $cacheFactory, auth) {
+        var apiCache = $cacheFactory('apiCache');
         var service = {
             addContact: addContact,
             getList: getList,
@@ -13,13 +14,14 @@
         };
         var req = {
             url: 'http://challenge.acstechnologies.com/api/contact/',
-            headers: { 'X-Auth-Token': auth.TOCKEN },
+            headers: { 'X-Auth-Token': auth.TOCKEN }
         }
         return service;
         function addContact() {
 
         }
         function getList() {
+            var data = apiCache.get('contactList');
             var tmpReq = jQuery.extend(true, {}, req);
             tmpReq.method = 'GET';
             tmpReq.data = {
@@ -28,12 +30,18 @@
                 "desc": true,
                 "page": 1
             }
-            return $http(tmpReq).then(onSuccess, onFailure);
+            if (data) {
+                logger.debug({ from: 'api.js', message: 'Using Cached Data for Contact List' });
+                return $q.resolve(data);
+            } else
+                return $http(tmpReq).then(onSuccess, onFailure);
             function onSuccess(response) {
-                if(response.status == 200 && response.data.total > 0){
+                if (response.status == 200 && response.data.total > 0) {
                     logger.debug({ from: 'api.js', message: 'API Status:' + response.status + ' Got the list of contacts' });
+                    apiCache.put('contactList', response.data);
+                    console.log(apiCache.get('contactList'));
                     return response.data;
-                }else{
+                } else {
                     logger.error({ from: 'api.js', message: 'API Status:' + response.status + ' Could not get list of contacts' });
                     logger.debug({ from: 'api.js', message: response });
                     return $q.reject(response.data);
@@ -46,13 +54,19 @@
             }
         }
         function getContact(contactId) {
-            var tmpReq = jQuery.extend(true,{},req);
+            var tmpReq = jQuery.extend(true, {}, req);
+            var data = apiCache.get('contact' + contactId);
             tmpReq.method = 'GET';
             tmpReq.url = tmpReq.url + contactId;
-            return $http(tmpReq).then(onSuccess, onFailure);
+            if (data) {
+                logger.debug({ from: 'api.js', message: 'Using Cached Data for Contact ' + contactId });
+                return $q.resolve(data);
+            }else
+                return $http(tmpReq).then(onSuccess, onFailure);
             function onSuccess(response) {
                 if (response.status == 200) {
                     logger.debug({ from: 'api.js', message: 'API Status:' + response.status + ' Got the contact with id ' + contactId });
+                    apiCache.put('contact' + contactId, response.data);
                     return response.data;
                 } else {
                     logger.error({ from: 'api.js', message: 'API Status:' + response.status + ' Could not get the contact with id ' + contactId });
