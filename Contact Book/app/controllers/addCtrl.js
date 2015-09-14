18f49b152f1fbc0ebacda5e15caf6e526c79d4b3
Will Dixon <dixonwille@hotmail.com>
2015-09-14 00:03:18 -0400

    Form Error Bug when changing previously submitted invalid data

--- Contact Book/app/controllers/addCtrl.js
+++ Contact Book/app/controllers/addCtrl.js
@@ -9,7 +9,10 @@
         vm.clear = clear;
         vm.home = home;
         vm.regExp = regexpConst;
+        resetFixed();
+        $scope.$watchCollection('vm.contact', contChange);
         function create() {
+            resetFixed();
             if ($scope.addForm.$valid) {            /*Unifies all contacts. If user puts a value and erases it reads '' not null.*/
                 for (var prop in vm.contact) {
                     if (vm.contact[prop] === '') {
@@ -34,6 +37,50 @@
             $scope.addForm.$setUntouched();
             $scope.addForm.$setValidity();
             $scope.addForm.$submitted = false;
+        }
+        function contChange(newContact) {
+            var count = 0;
+            if ($scope.addForm.firstName.$valid)
+                vm.fixed.firstName = true;
+            if ($scope.addForm.lastName.$valid)
+                vm.fixed.lastName = true;
+            if ($scope.addForm.url.$valid)
+                vm.fixed.url = true;
+            if ($scope.addForm.address.$valid)
+                vm.fixed.address = true;
+            if ($scope.addForm.city.$valid)
+                vm.fixed.city = true;
+            if ($scope.addForm.state.$valid)
+                vm.fixed.state = true;
+            if ($scope.addForm.zip.$valid)
+                vm.fixed.zip = true;
+            if ($scope.addForm.email.$valid)
+                vm.fixed.email = true;
+            if ($scope.addForm.phone.$valid)
+                vm.fixed.phone = true;
+            if ($scope.addForm.work.$valid)
+                vm.fixed.work = true;
+            for (var prop in vm.fixed) {
+                if (vm.fixed[prop] == true)
+                    count += 1;
+            }
+            if (count == 10)
+                vm.fixedForm = true;
+        }
+        function resetFixed() {
+            vm.fixed = {
+                firstName: false,
+                lastName: false,
+                url: false,
+                address: false,
+                city: false,
+                state: false,
+                zip: false,
+                email: false,
+                phone: false,
+                work: false
+            }
+            vm.fixedForm = false;
         }
     }
 })();
--- Contact Book/app/controllers/contCtrl.js
+++ Contact Book/app/controllers/contCtrl.js
@@ -12,6 +12,8 @@
         vm.reset = reset;
         vm.regExp = regexpConst;
         api.getContact(vm.contactId).then(onSuccess);
+        resetFixed();
+        $scope.$watchCollection('vm.contact',contChange);
         function onSuccess(response) {
             vm.contact = response;
             originalContact = angular.merge({}, contactConst.CONTACTOBJ, vm.contact);       /*Unifies contact to what is present in API database.*/
@@ -20,6 +22,7 @@
             $location.path('/home');
         }
         function update() {
+            resetFixed();
             if ($scope.editForm.$valid) {           /*Unifies all contacts. If user puts a value and erases it reads '' not null.*/
                 for (var prop in vm.contact) {
                     if (vm.contact[prop] === '') {
@@ -43,10 +46,55 @@
         }
         function reset() {
             vm.contact = angular.copy(originalContact);
+            resetFixed();
             $scope.createForm.$setPristine();
             $scope.createForm.$setUntouched();
             $scope.createForm.$setValidity();
             $scope.createForm.$submitted = false;
+        }
+        function contChange(newContact) {
+            var count = 0;
+            if ($scope.editForm.firstName.$valid)
+                vm.fixed.firstName = true;
+            if ($scope.editForm.lastName.$valid)
+                vm.fixed.lastName = true;
+            if ($scope.editForm.url.$valid)
+                vm.fixed.url = true;
+            if ($scope.editForm.address.$valid)
+                vm.fixed.address = true;
+            if ($scope.editForm.city.$valid)
+                vm.fixed.city = true;
+            if ($scope.editForm.state.$valid)
+                vm.fixed.state = true;
+            if ($scope.editForm.zip.$valid)
+                vm.fixed.zip = true;
+            if ($scope.editForm.email.$valid)
+                vm.fixed.email = true;
+            if ($scope.editForm.phone.$valid)
+                vm.fixed.phone = true;
+            if ($scope.editForm.work.$valid)
+                vm.fixed.work = true;
+            for (var prop in vm.fixed) {
+                if (vm.fixed[prop] == true)
+                    count += 1;
+            }
+            if (count == 10)
+                vm.fixedForm = true;
+        }
+        function resetFixed() {
+            vm.fixed = {
+                firstName: false,
+                lastName: false,
+                url: false,
+                address: false,
+                city: false,
+                state:false,
+                zip: false,
+                email: false,
+                phone: false,
+                work: false
+            }
+            vm.fixedForm = false;
         }
     }
 })();
